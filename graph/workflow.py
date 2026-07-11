c945ff8fd8cbb8c333132ad8f2d32d804f233e56
hirakpal <60995278+hirakpal@users.noreply.github.com>
2026-07-11 17:33:55 +0530

    Update workflow.py

--- graph/workflow.py
+++ graph/workflow.py
@@ -1,6 +1,9 @@
 from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
 from .state import TripState
+from backend.agents.luna import luna_chat_node
+from backend.agents.explorer import explorer_node
+from backend.agents.hitl import hitl_node
 
 def router_node(state: TripState):
     """Decides next step based on last message"""
@@ -20,10 +23,18 @@ def build_graph():
     workflow = StateGraph(TripState)
     
     workflow.add_node("router", router_node)
-    # More nodes will be added in next steps
+    workflow.add_node("luna_chat", luna_chat_node)
+    workflow.add_node("explorer", explorer_node)
+    workflow.add_node("hitl", hitl_node)
     
     workflow.set_entry_point("router")
     workflow.add_conditional_edges("router", lambda s: s.get("next", END))
+
+    # Return edges
+    workflow.add_edge("planner", "hitl")
+    workflow.add_edge("hitl", "router")
+    workflow.add_edge("luna_chat", "router")
+    workflow.add_edge("explorer", "router")
     
     memory = MemorySaver()
     return workflow.compile(checkpointer=memory)
